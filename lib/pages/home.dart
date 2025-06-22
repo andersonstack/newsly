08e331384fe0783af7c182ed031776072f2b7e1f
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-22 13:44:44 -0300

    style: adicionando o layout da Home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import '../interface/article.dart';
+import '../components/card.dart';
 
 class Home extends HookWidget {
+  final ValueNotifier<List<Map<String, dynamic>>> articlesTechnology;
+
+  const Home({super.key, required this.articlesTechnology});
+
   @override
   Widget build(BuildContext context) {
-    final currentIndex = useState(0); // controla o botão ativo
+    final screenHeight = MediaQuery.of(context).size.height;
+    final currentIndex = useState(0);
+
+    final List<Map<String, dynamic>> articlesMap = articlesTechnology.value;
+    final List<Article> articles = articlesMap
+        .map((e) => Article.fromJson(e))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(title: const Text("Today's News")),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: const TextField(
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
               decoration: InputDecoration(
                 hintText: "Search Notice",
                 suffixIcon: Icon(Icons.search),
@@ -20,20 +32,55 @@ class Home extends HookWidget {
               ),
             ),
           ),
-          Container(width: double.infinity, height: 500, color: Colors.amber),
-          Row(
-            children: [
-              Container(
-                width: 400,
-                height: 300,
-                color: const Color.fromARGB(255, 255, 38, 0),
-              ),
-              Container(
-                width: 150,
-                height: 200,
-                color: const Color.fromARGB(255, 15, 0, 223),
-              ),
-            ],
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned(
+                  child: Image.network(
+                    articles[0].urlToImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                // Lista de artigos
+                Positioned(
+                  top: screenHeight * 0.32,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: ListView.builder(
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: articles.length,
+                    itemBuilder: (context, index) {
+                      if (index != 0) {
+                        return CardArticle(article: articles[index]);
+                      }
+                      return const SizedBox.shrink();
+                    },
+                  ),
+                ),
+
+                // Texto sobreposto em destaque
+                Positioned(
+                  top: screenHeight * 0.27,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: screenHeight * 0.05,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Color(0xFF0077CC)),
+                    child: Text(
+                      "Follow the latest technology news",
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
